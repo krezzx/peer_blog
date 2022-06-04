3a292a0c5520e09cc8f2f684ff85006e437cb9fb
krrezx <krritesh2001@gmail.com>
2022-06-04 16:16:30 +0530

    added tables

--- app.py
+++ app.py
@@ -12,15 +12,14 @@ import os
 
 
 app=Flask(__name__)
-api=Api(app)
 app.config['SECRET_KEY']='8efde650d0e727ef697bb75adb2a114a'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///userData.db'
 app.config['SQLALCHEMY_BINDS']={
-    'posts':'sqlite:///posts.db'
+    'posts':'sqlite:///posts.db',
+    'comment':'sqlite:///comment.db',
+    'vote':'sqlite:///vite.db'
 }
-UPLOAD_FOLDER = 'static/uploads/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False 
 
 db=SQLAlchemy(app)
@@ -33,26 +32,15 @@ login_manager.login_message_category='info'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-#centralized
-
-class centralized(Resource):
-      def get(self,emailid,password):
-        user=User.query.filter_by(email=emailid).first()
-        if user and bcrypt.check_password_hash(user.password,password):
-            return json.dumps({"rollNo":user.id,"userName":user.username,"email":user.email,"status":True})
-        return json.dumps({"status":False})
-
-api.add_resource(centralized,"/centralized/<string:emailid>/<string:password>")
-
 
 
 #database model
 class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(20),nullable=False)
+    username=db.Column(db.String(20),nullable=False,unique=True)
     email=db.Column(db.String(120),nullable=False)
     password=db.Column(db.String(60),nullable=False)
-    
+    colleague=db.Column(db.String(500),default=" ")
     def __repr__(self):
         return f"User('{self.id}','{self.username}','{self.email}')"
 
@@ -60,11 +48,16 @@ class User(db.Model,UserMixin):
 class Posts(db.Model):
     __bind_key__='posts'
     id=db.Column(db.Integer,primary_key=True)
-    roll_no=db.Column(db.Integer)
+    uid=db.Column(db.Integer)
     caption=db.Column(db.String(200))
     username=db.Column(db.String(40))
-    appr=db.Column(db.Integer,default=0)
-    pic=db.Column(db.String(150))
+
+class Comment(db.Model):
+    __bind_key__='comment'
+    id=db.Column(db.Integer,primary_key=True)
+    com=db.Column(db.String(150))
+    uid=db.Column(db.Integer)
+    pid=db.Column(db.Integer)
 
 
 from routes import *
---
+++ routes.py
@@ -0,0 +1,144 @@
+from forms import RegistrationForm,LoginForm
+from flask import render_template,flash,redirect,url_for,request
+from app import db,bcrypt,app,User,Posts
+from flask_login import current_user,login_required,logout_user,login_user
+from werkzeug.utils import secure_filename
+import os
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+ return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html',login=True)
+
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm(request.form)
+    if request.method=='POST' and form.validate():
+        hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user=User(id=form.RollNo.data,username=form.userName.data,email=form.emailId.data,password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created successfully ,you may login now!",'success')
+        return redirect(url_for('login'))
+    return render_template('register.html',form=form)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        if(current_user.id==205121082):
+            pass
+        return render_template('index.html',login=False,post="Hello "+ current_user.username + ", Welcome to INNOVAC'22")
+    form=LoginForm(request.form)
+    if request.method=='POST' and form.validate():
+        user=User.query.filter_by(email=form.emailId.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user,remember=form.remember.data)
+            print(current_user.username)
+            return render_template('index.html',login=False,post="Hello "+ current_user.username + ", Welcome to INNOVAC'22")
+        else:
+            flash('Login Unsuccessful. Please check either Email or Password','danger')
+    return render_template('login.html',form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('hello'))
+
+@app.route('/media')
+@login_required
+def media():
+    return render_template('home.html',user=current_user.id) 
+@app.route('/feed')
+def feed():
+    P=Posts.query.with_entities(Posts.id).all()
+    P.reverse()
+    p=dict()
+    for i in P:
+        post=Posts.query.filter_by(id=i[0]).first()
+        if(post.appr==1):
+            a=[]
+            a.append(post.caption)
+            a.append(post.username)
+            a.append(post.roll_no)
+            picname='uploads/'+post.pic
+            p[picname]=a
+    print(current_user.id)
+    return render_template('feed.html',dict=p,cid=current_user.id)
+
+
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+@app.route('/adminfeed')
+@login_required
+def adfeed():
+    P=Posts.query.with_entities(Posts.id).all()
+    P.reverse()
+    print(P)
+    p=dict()
+    for i in P:
+        post=Posts.query.filter_by(id=i[0]).first()
+        print(post.id)
+        a=[]
+        a.append(post.caption)
+        a.append(post.username)
+        a.append(post.roll_no)
+        apr=False
+        if(post.appr==1):
+            apr=True
+        a.append(apr)
+        picname='uploads/'+post.pic
+        p[picname]=a
+    print(p)
+    return render_template('admin.html',dict=p)
+
+
+@app.route('/uploader',methods=['GET','POST'])
+@login_required
+def uploader():
+    file = request.files['inputFile']
+    #name tag of form
+    roll_no = current_user.id
+    caption=request.form['caption']
+    username=current_user.username
+    filename=str(roll_no)+'_'+file.filename
+    filename = secure_filename(filename)
+  
+    if file and allowed_file(file.filename):
+       file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  
+       newFile = Posts(pic=filename, roll_no=roll_no,caption=caption,username=username)
+       db.session.add(newFile)
+       db.session.commit()
+    #    flash('File successfully uploaded ' + file.filename + ' to the database!')
+       return redirect(url_for('feed'))
+    # else:
+    #    flash('Invalid Uplaod only txt, pdf, png, jpg, jpeg, gif') 
+    return redirect(url_for('feed'))
+      
+@app.route('/review/<picid>',methods=['GET','POST']) 
+@login_required
+def approve(picid):
+    p=Posts.query.filter_by(pic=picid).first()
+    p.appr=1
+    db.session.commit()
+    return redirect(url_for('adfeed'))
+
+@app.route('/remove/<picid>',methods=['GET','POST']) 
+@login_required
+def remove(picid):
+    p=Posts.query.filter_by(pic=picid).first()
+    p.appr=0
+    db.session.commit()
+    return redirect(url_for('adfeed'))
